6516dd3ce729baf7b91b905d025202f971471b36
Nelson <nelson.soto.c05@gmail.com>
2023-05-07 19:39:29 -0400

    feat: add service to fetch products

---
+++ src/hooks/useProducts.ts
@@ -0,0 +1,31 @@
+import { useState, useCallback } from 'react';
+import { Product } from '../types/Product';
+
+async function fetchProducts(): Promise<Product[]> {
+  const response = await window.fetch('https://api.escuelajs.co/api/v1/products');
+  const data = await response.json();
+  if (response.ok) return data;
+  const error = new Error('Error fetching products');
+  return Promise.reject(error);
+}
+
+export const useProducts = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<Error | string | null>(null);
+
+  const getProducts = useCallback(async () => {
+    setLoading(true);
+    try {
+      const products = await fetchProducts();
+      setProducts(products);
+    } catch (err) {
+      const error = err as Error;
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  return { products, loading, error, getProducts };
+}
